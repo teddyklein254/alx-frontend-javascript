d53d2f3ef7ff6a4e83be31ea88b34d1ef9b5e9c0
Teddy papa <paapateddyetyang@gmail.com>
2024-10-02 18:59:55 +0300

    add

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,16 @@
+// 1-promise.js
+const getFullResponseFromAPI = (success) => {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success'
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+};
+
+export default getFullResponseFromAPI;
+
