9932c878ba7a6275f2c9699a1e37189df07cc14f
Teddy papa <paapateddyetyang@gmail.com>
2024-10-07 19:55:20 +0300

    great

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+export GetListStudents (){
+    return [
+        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+        { id: 2, firstName: 'James', location: 'Columbia' },
+        { id: 5, firstName: 'Serena', location: 'San Francisco' }
+        ];
+}
+const students = GetListStudents;
+console.log(students)
