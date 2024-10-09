55e931feefc43eadfb4fc8de4df0f700a0b41e92
Teddy papa <paapateddyetyang@gmail.com>
2024-10-09 09:42:43 +0300

    goon

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,7 @@
-export GetListStudents (){
-    return [
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' }
-        ];
+export default function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
-const students = GetListStudents;
-console.log(students)
--- 0x03-ES6_data_manipulation/README.md
+++ 0x03-ES6_data_manipulation/README.md
@@ -1,1 +1,1 @@
-my Data manipulation for getting list in array
+## 0x03. ES6 data manipulation
